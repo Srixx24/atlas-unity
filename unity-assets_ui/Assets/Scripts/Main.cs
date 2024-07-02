fa54f0c959476cffdf246bf43d5551028a1a386b
unknown <jacolynlovins@gmail.com>
2024-07-01 22:40:25 -0500

    Edits

---
+++ unity-assets_ui/Assets/Scripts/Main.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    // Button click logic
+    public Button playButton;
+    public Button quitButton;
+
+    void Start()
+    {
+        playButton.onClick.AddListener(PlayGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+    // Start maze game
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+
+    // Quit game
+    public void QuitGame()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+}
--- unity-assets_ui/Assets/Scripts/MainMenu.cs
+++ unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -1,27 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
-    // Button click logic
-    public Button playButton;
+    public Button lvl1;
+    public Button lvl2;
+    public Button lvl3;
     public Button quitButton;
+    public Button optionsButton;
 
-    void Start()
+    private void Start()
     {
-        playButton.onClick.AddListener(PlayGame);
+        lvl1.onClick.AddListener(delegate {LevelSelect(0); });
+		lvl2.onClick.AddListener(delegate {LevelSelect(1); });
+        lvl3.onClick.AddListener(delegate { LevelSelect(2); });
         quitButton.onClick.AddListener(QuitGame);
-    }
-    // Start maze game
-    public void PlayGame()
-    {
-        SceneManager.LoadScene("Level01");
+        optionsButton.onClick.AddListener(OptionsButton);
     }
 
-    // Quit game
+    public void LevelSelect(int level)
+    {
+        switch (level)
+        {
+            case 1:
+                SceneManager.LoadScene("Level01");
+                break;
+            case 2:
+                SceneManager.LoadScene("Level02");
+                break;
+            case 3:
+                SceneManager.LoadScene("Level03");
+                break;
+        }
+    }
+
+    // Loads the options menu
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    // Exit application
     public void QuitGame()
     {
         Debug.Log("Quit game");
